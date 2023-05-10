3e0899cc6da6895713c90830346cd08b20e6ff2a
Ivan Souza <ivan.lmedeiros76@gmail.com>
2023-05-10 15:05:25 -0400

    Create usuarios.js

---
+++ classes/usuarios.js
@@ -0,0 +1,36 @@
+const user = {
+    nome: "Ivan",
+    email: "ivan@email.com",
+    nascimento: "2021/03/04",
+    role: "admin",
+    ativo: true,
+
+    exibirDados: function(){ 
+        console.log(this.nome, this.email)
+    } 
+}
+
+// user.exibirDados()
+
+// const exibir = function (){
+//     console.log(this.nome)
+// };
+
+// const exibirNome = exibir.bind(user);
+
+// exibirNome();
+
+const admin = { 
+    nome: "Mariana",
+    email: "mariana@email.com",
+    role: "admin",
+    criarCurso(){
+        console.log('curso criado')
+    }
+}
+
+
+Object.setPrototypeO(admin, user)
+
+admin.criarCurso();
+admin.exibirDados();
